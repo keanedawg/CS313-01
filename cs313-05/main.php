f8140aea39563bd2a93f220aaf622c1a7bb6bd83
keanedawg <cameronfife@hotmail.com>
2018-02-15 02:13:06 -0700

    added main.php

---
+++ cs313-05/main.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+$dbUrl = getenv('DATABASE_URL');
+$message = "";
+$db = Null;
+
+
+if (empty($dbUrl)) {
+	try
+	{
+		$host = "localhost";
+		$user = 'web_user';
+		$password = 'fake-password';
+		$db = new PDO('pgsql:host=localhost;dbname=scripture', $user, $password);
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex)
+	{
+	  echo 'Error!: ' . $ex->getMessage();
+	  die();
+	}
+	$message = "connected to local DB!";
+}
+else{
+
+	$dbopts = parse_url($dbUrl);
+
+	$dbHost = $dbopts["host"];
+	$dbPort = $dbopts["port"];
+	$dbUser = $dbopts["user"];
+	$dbPassword = $dbopts["pass"];
+	$dbName = ltrim($dbopts["path"],'/');
+
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	$message = "successfully connected on heroku!";
+}
+$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+?>
+
+
+<h1>Rate My Housing</h1>
+<i>The best way to find good housing</i>
