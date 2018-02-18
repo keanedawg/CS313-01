d36bdb55cc43227d45217516e3fba968f58ce04d
keanedawg <cameronfife@hotmail.com>
2018-02-18 13:44:12 -0700

    Basic main layout

--- cs313-05/main.php
+++ cs313-05/main.php
@@ -2,18 +2,7 @@
 session_start();
 require('dbConnect.php');
 $myDb = get_db();
-
-
-// $movieId = $_GET["movieId"];
-// $stmt->bindValue(':theid', $movieId, PDO::PARAM_INT);
-
-$stmt = $myDb->prepare('SELECT * FROM houses');
-$stmt->execute();
-$houses = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,10 +10,11 @@ $houses = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <link rel="stylesheet" href="basic.css">
 </head>
 <body>
-<?php require 'header.php'; ?>
 
-<div class="main">
-    
+<?php require 'header.php'; ?>
+<?php require 'sidebar.php'; ?>
+
+<div class="main"> 
     <h1>Welcome, click on any of the houses to get started!</h1>
 </div>
 </body>
