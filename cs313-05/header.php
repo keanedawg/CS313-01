b248f602b46bb5dadb89b662b6eb19463983ff22
keanedawg <cameronfife@hotmail.com>
2018-02-18 16:21:04 -0700

    header links to main

--- cs313-05/header.php
+++ cs313-05/header.php
@@ -1,4 +1,4 @@
 <div class="top">
-    <img src="http://jitariustudio.com/wp-content/uploads/2014/05/home_icon.png" class="home-nav">
+    <a href="main.php"><img src="http://jitariustudio.com/wp-content/uploads/2014/05/home_icon.png" class="home-nav"></a>
     <h3 class="title">Rate My Housing</h3>
 </div>
