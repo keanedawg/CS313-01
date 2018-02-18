09ba744c1c97c26db2cdb74b38c94bf76225fce4
keanedawg <cameronfife@hotmail.com>
2018-02-17 18:17:57 -0700

    Reviews now load up

--- cs313-05/house.php
+++ cs313-05/house.php
@@ -4,32 +4,53 @@ require('dbConnect.php');
 $myDb = get_db();
 
 
-$movieId = $_GET["movieID"];
-// $stmt->bindValue(':theid', $movieId, PDO::PARAM_INT);
+$houseId = $_GET["house"];
 
-$stmt = $myDb->prepare('SELECT * FROM house_ratings');
+$stmt = $myDb->prepare("select * from houses where id = :theid ;");
 $stmt->execute();
-$houses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$house = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$stmt = $myDb->prepare("select * from house_reviews WHERE house_id = :theid ;");
+$stmt->bindValue(':theid', $houseId, PDO::PARAM_INT);
+$stmt->execute();
+$reviews = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo "put something in here"; ?></title>
+	<title><?php echo $house["name"]; ?></title>
 </head>
 <body>
 <h1>Rate My Housing</h1>
-<i>The best way to find good housing</i>
+<i>The best way to find good housing</i><br>
+<h2><?php echo $house; ?></h2>
+
+<a href="main.php">go back</a> 
+
 	<ul>
     <?php
-    foreach ($houses as $house)
+    echo "<hr size=2>";
+    foreach ($reviews as $review)
     {
-        $name = $house["score"];
-        $address = $house["address"];
-        $picture = $house["picture"];
-        echo "<li><p>$name</p></li>";
-        echo "<img src=\"$picture\">";
-        echo "<li><p>$address</p></li>";
+        $score = $review["score"];
+        $commentary = $review["commentary"];
+        $recommended = $review["recommended"];
+        echo "<p>Anon gave it a <b>$score</b> and ";
+        echo "would";
+        if ($recommended) {
+            echo " <b>recommend</b>";
+        }
+        else {
+            echo " <b>not recommend</b>";
+        }
+        echo " it to a friend.</p>";
+        
+        echo "<h3>Anon says: </h3>";
+        echo "<p>$commentary</p>";
+        echo "<hr size=2>";
     }
     ?>
 	</ul>
