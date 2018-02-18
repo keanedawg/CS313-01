cb7bda3662364d533356a72cff96b4beefacf38c
keanedawg <cameronfife@hotmail.com>
2018-02-18 13:45:05 -0700

    made sidebar a component

--- cs313-05/sidebar.php
+++ cs313-05/sidebar.php
@@ -1,28 +1,9 @@
 <?php
-session_start();
-require('dbConnect.php');
-$myDb = get_db();
-
-
-// $movieId = $_GET["movieId"];
-// $stmt->bindValue(':theid', $movieId, PDO::PARAM_INT);
-
 $stmt = $myDb->prepare('SELECT * FROM houses');
 $stmt->execute();
 $houses = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Rate My Housing</title>
-    <link rel="stylesheet" href="basic.css">
-</head>
-<body>
-<h1>Rate My Housing</h1>
-<i>The best way to find good housing</i>
 <div class="left">
 	<ul>
     <?php
@@ -36,6 +17,4 @@ $houses = $stmt->fetchAll(PDO::FETCH_ASSOC);
     ?>
 	</ul>
 </div>
-</body>
-</html>
 
