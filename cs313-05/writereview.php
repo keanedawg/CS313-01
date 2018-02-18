2256388ad90b9ba0915e0660843b972f1cb6bf84
keanedawg <cameronfife@hotmail.com>
2018-02-18 16:46:23 -0700

    Added writeReviews section

---
+++ cs313-05/writereview.php
@@ -0,0 +1,25 @@
+<!-- Not actually used, just a template for creating new pages -->
+
+<?php
+session_start();
+require('dbConnect.php');
+$myDb = get_db();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rate My Housing</title>
+    <link rel="stylesheet" href="basic.css">
+</head>
+<body>
+    <?php 
+        require 'header.php';
+        require 'sidebar.php';
+    ?>
+    <div class="main">
+        <h1>Coming Soon!</h1>
+    </div>
+</body>
+</html>
+
