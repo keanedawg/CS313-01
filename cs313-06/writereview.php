dae191384892d4903edbab6bb67985dee50358c5
keanedawg <cameronfife@hotmail.com>
2018-02-19 15:21:22 -0700

    set up simple form

--- cs313-06/writereview.php
+++ cs313-06/writereview.php
@@ -1,6 +1,19 @@
 <?php
 require('dbConnect.php');
 $myDb = get_db();
+
+$houseId = $_GET["house"];
+
+$stmt = $myDb->prepare("SELECT name, id FROM houses WHERE id = :theid ;");
+$stmt->bindValue(':theid', $houseId, PDO::PARAM_INT);
+$stmt->execute();
+$house = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+$stmt = $myDb->prepare("SELECT * FROM employees WHERE house_id = :theid;");
+$stmt->bindValue(':theid', $houseId, PDO::PARAM_INT);
+$stmt->execute();
+$employees = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -14,8 +27,13 @@ $myDb = get_db();
         require 'header.php';
         require 'sidebar.php';
     ?>
+    
     <div class="main">
-        <h1>Coming Soon!</h1>
+    <h1>foo</h1>
+        <form>
+            Would you recommend it?<input type="checkbox"><br>
+            How would you rate it overall?<input type="text"><br>
+        </form>
     </div>
 </body>
 </html>
