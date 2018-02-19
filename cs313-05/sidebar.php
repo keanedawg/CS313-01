f88f9878c74df30897cefdabbb8efcfa68131701
keanedawg <cameronfife@hotmail.com>
2018-02-18 17:54:55 -0700

    Cleaned up query to only include necessary columns

--- cs313-05/sidebar.php
+++ cs313-05/sidebar.php
@@ -1,7 +1,14 @@
 <?php
-$stmt = $myDb->prepare('SELECT * FROM houses
-LEFT JOIN (SELECT house_id, trunc(avg(score), 1) FROM house_reviews GROUP BY house_id) AS r
-ON houses.id = r.house_id;');
+$stmt = $myDb->prepare('SELECT
+                            h.id, h.name,
+                            r.avg_score
+                        FROM houses h
+                        LEFT JOIN (
+                            SELECT house_id, trunc(avg(score), 1) avg_score
+                            FROM house_reviews 
+                            GROUP BY house_id
+                        ) AS r
+                        ON h.id = r.house_id;');
 $stmt->execute();
 $complexes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -13,7 +20,7 @@ $complexes = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $complexID = $complex["id"];
         echo "<a href=\"house.php?house=$complexID\"><div class=\"house-row\">";
         $name = $complex["name"];
-        $score = $complex["trunc"];
+        $score = $complex["avg_score"];
         echo "<p class=\"house-row-title\">$name</p>";
         echo "<p class=\"house-row-address\">$score</p>";
         echo "</div></a>";
