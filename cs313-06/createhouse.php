7d2bf33cbf11a68fc35293c5e89193d435dce004
keanedawg <cameronfife@hotmail.com>
2018-02-20 05:30:45 -0700

    Created createemployee and createhouse page

---
+++ cs313-06/createhouse.php
@@ -0,0 +1,21 @@
+<?php
+require('dbConnect.php');
+$myDb = get_db();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rate My Housing</title>
+    <link rel="stylesheet" href="basic.css">
+</head>
+<body>
+    <?php 
+        require 'header.php';
+        require 'sidebar.php';
+    ?>
+    <div class="main">
+        <h1>Coming Soon!</h1>
+    </div>
+</body>
+</html>
