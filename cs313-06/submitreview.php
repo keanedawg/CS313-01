2b5a3d45986bc15f45232b76523235eaae761138
keanedawg <cameronfife@hotmail.com>
2018-02-20 04:00:43 -0700

    Created submitreview page

---
+++ cs313-06/submitreview.php
@@ -0,0 +1,15 @@
+<?php
+    require('dbConnect.php');
+    $myDb = get_db();
+
+    $houseId = $_POST["houseid"];
+
+    // This is maybe a bit hackish. I'm using a query to find employee POST parameters
+    $stmt = $myDb->prepare("SELECT name, id FROM employees WHERE house_id = :theid;");
+    $stmt->bindValue(':theid', $houseId, PDO::PARAM_INT);
+    $stmt->execute();
+    $employees = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<p>$houseID</p>";
+
+?>
