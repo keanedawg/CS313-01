9d3a782d3ff89a017b3469dfb2ae07317e811628
keanedawg <cameronfife@hotmail.com>
2018-02-21 03:12:26 -0700

    added more selects

--- cs313-06/adminHousing.php
+++ cs313-06/adminHousing.php
@@ -13,10 +13,15 @@ $stmt->bindValue(':theid', $houseId, PDO::PARAM_INT);
 $stmt->execute();
 $employees = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$stmt = $myDb->prepare("SELECT id, name FROM employees;");
+$stmt = $myDb->prepare("SELECT id, house_id, commentary FROM house_reviews;");
 $stmt->bindValue(':theid', $houseId, PDO::PARAM_INT);
 $stmt->execute();
-$employees = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$hreviews = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt = $myDb->prepare("SELECT * FROM employee_reviews;");
+$stmt->bindValue(':theid', $houseId, PDO::PARAM_INT);
+$stmt->execute();
+$ereviews = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -33,10 +38,10 @@ $employees = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <div class="main">
         <h1>Welcome <?php echo $_SESSION["username"];?></h1>
         <h3>Delete House</h3>
-        <form>
+        <form action="delete.php" method="GET">
             <select name="house">
                 <?php
-                    foreach($complexes as $complex) {
+                    foreach ($complexes as $complex) {
                         $complexId = $complex["id"];
                         $complexName = $complex["name"];
                         echo "<option value=$complexId>$complexName</option>";
@@ -46,32 +51,52 @@ $employees = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <input class="submit-delete" type="submit" value="Delete">
         </form>
         <hr>
-        <form>
-            <select name="employee_review">
+        <h3>Delete House Review</h3>
+        <form action="delete.php" method="GET">
+            houseid - commentary
+            <select name="house_review">
                 <?php
-                    foreach($complexes as $complex) {
-                        $complexId = $complex["id"];
-                        $complexName = $complex["name"];
-                        echo "<option value=$complexId>$complexName</option>";
+                    foreach($hreviews as $hreview) {
+                        $rId = $hreview["id"];
+                        $rHid = $hreview["house_id"];
+                        $rCommentary = $hreview["commentary"];
+                        echo "<option value=$rId>$rHid - $rCommentary</option>";
                     }
                 ?>
             </select><br>
             <input class="submit-delete" type="submit" value="Delete">
         </form>
         <hr>
-        <form>
+        <h3>Delete Employee</h3>
+        <form action="delete.php" method="GET"> 
+            Name
             <select name="employee">
                 <?php
-                    foreach($complexes as $complex) {
-                        $complexId = $complex["id"];
-                        $complexName = $complex["name"];
-                        echo "<option value=$complexId>$complexName</option>";
+                    foreach($employees as $employee) {
+                        $empId = $employee["id"];
+                        $empName = $employee["name"];
+                        echo "<option value=$empId>$empName</option>";
                     }
                 ?>
             </select><br>
             <input class="submit-delete" type="submit" value="Delete">
         </form>
         <hr>
+        <h3>Delete Employee Review</h3>
+        <form action="delete.php" method="GET">
+            employeeId - Review Score
+            <select name="employee_review">
+                <?php
+                    foreach($ereviews as $ereview) {
+                        $erevId = $ereview["id"];
+                        $erevEmpId = $ereview["employee_id"];
+                        $erevScore = $ereview["score"];
+                        echo "<option value=$erevId>$erevEmpId - $erevScore</option>";
+                    }
+                ?>
+            </select><br>
+            <input class="submit-delete" type="submit" value="Delete">
+        </form>
     </div>
 </body>
 </html>
