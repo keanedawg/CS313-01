d1f21bf73bea2dc5acd70504e009646d5b89c784
keanedawg <cameronfife@hotmail.com>
2018-02-21 03:26:16 -0700

    changed redirect link

--- cs313-06/delete.php
+++ cs313-06/delete.php
@@ -4,22 +4,34 @@ require('dbConnect.php');
 $myDb = get_db();
 
 if (!isset($_SESSION["username"])) {
-    header("Location: main.php");
+    header("Location: login.php");
     exit();
 }
 
 if (isset($_SESSION["house"])) {
     $stmt = $myDb->prepare("DELETE FROM houses WHERE id = :theid ;");
+    $id = $_SESSION["house"];
 }
 else if (isset($_SESSION["house_review"])) {
     $stmt = $myDb->prepare("DELETE FROM house_reviews WHERE id = :theid ;");
+    $id = $_SESSION["house_review"];
 }
 else if (isset($_SESSION["employee"])) {
     $stmt = $myDb->prepare("DELETE FROM employee WHERE id = :theid ;");
+    $id = $_SESSION["employee"];
 }
 else if (isset($_SESSION["employee_review"])) {
     $stmt = $myDb->prepare("DELETE FROM employee_review WHERE id = :theid ;");
+    $id = $_SESSION["employee_review"];
 }
+else {
+    header("Location:adminHousing.php");
+    exit(); 
+}
+$stmt->bindValue(':theid', $id, PDO::PARAM_INT);
+$stmt->execute();
 
 
+header("Location:adminHousing.php");
+exit();
 ?>
