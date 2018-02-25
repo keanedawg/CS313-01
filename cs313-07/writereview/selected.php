ffa9f0b18a707a00aa358173fedd945bb8cb055d
keanedawg <cameronfife@hotmail.com>
2018-02-24 23:08:05 -0700

    updated jquerry code to https (might fix issues in Heroku)

--- cs313-07/writereview/selected.php
+++ cs313-07/writereview/selected.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
     // Check Radio-box
